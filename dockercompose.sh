91feff75a24bc4bbbcfb6e000e9ec61e4a0c53c8
Philip <phucvd.ce@gmail.com>
2017-04-28 01:21:00 +0700

    Update dockercompose.sh

--- dockercompose.sh
+++ dockercompose.sh
@@ -2,6 +2,7 @@
 set -e
 
 apt-get update -y
+apt-get install curl -y
 curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"
 
 ls -la
